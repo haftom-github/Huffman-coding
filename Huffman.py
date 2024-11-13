f96f0cf130db0db47c61956b3c8ab20e88fea634
Haftom <haftish.tsegay@gmail.com>
2024-11-13 18:39:46 +0300

    added Huffman tree generation

--- Huffman.py
+++ Huffman.py
@@ -1,4 +1,39 @@
 from collections import defaultdict
+from treee import Tree
+
+def generateTree(dct):
+    """
+    Generates a Huffman tree based on the provided weight dictionary.
+
+    This function takes a weight dictionary where each character's weight represents its frequency 
+    or probability, and builds the corresponding Huffman tree. The function repeatedly combines the 
+    two smallest weight trees from the forest until only one tree remains, which becomes the final 
+    Huffman tree.
+
+    :param dct: dict - A dictionary where keys are characters, and values are their corresponding weights 
+                 (e.g., {'A': 0.5, 'B': 0.2, 'C': 0.3}).
+    :return: Tree - The root of the generated Huffman tree, which represents the entire tree structure.
+    """
+    forest = []
+    for key in dct:
+        forest.append(Tree(key, dct[key]))
+
+    while len(forest) > 1:
+        # find the two smallest weight trees from the forest
+        smallWI = 0
+        for i in range(len(forest)):                                            #choose the Tree with the smallest weight
+            if forest[i].getweight() < forest[smallWI].getweight():
+                smallWI = i
+        right=forest.pop(smallWI)
+        smallWI=0
+        for i in range(len(forest)):                                            #choose the second smmallest weight tree to be combined with the above Tree to one tree
+            if forest[i].getweight() < forest[smallWI].getweight():
+                smallWI = i
+        left = forest.pop(smallWI)
+        weightsum = left.getweight() + right.getweight()
+        t = Tree(None, weightsum).concat(left, to='left').concat(right, to='right')         #concatinating the two smallest weight trees into one
+        forest.append(t)
+    return forest[0]
 
 def generateWeightDict(filename, enc='utf-8'):
     """
